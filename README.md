a7516c452687b074a927ff40ef02bc74435c25dd
proshik <proxoraleksandrovich@gmail.com>
2017-01-10 23:23:48 +0300

    Registry server

---
+++ README.md
@@ -0,0 +1,10 @@
+# Registry server
+
+[![Build Status](https://api.travis-ci.org/proshik/thinkclearly.svg?branch=master)](https://travis-ci.org/proshik/registry-server)
+[![codecov](https://codecov.io/gh/proshik/registry-server/branch/master/graph/badge.svg)](https://codecov.io/gh/proshik/registry-server)
+
+ Spring Cloud Registy server based on Netflix Eureka
+ 
+## Environment variables
+
+CONFIG_SERVICE_PORT - port of registry-server (example: 8761)
---
+++ src/main/java/ru/proshik/infrastructure/registry_server/RegistryApplication.java
@@ -0,0 +1,17 @@
+package ru.proshik.infrastructure.registry_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by proshik on 10.01.17.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class);
+    }
+}
